e7d9a9f99c9015dbd9c6a20d91a2b33e9e678335
Ben D <benjamin.d928@icloud.com>
2019-09-11 17:42:02 +0100

    Fixed graphdb error

--- db/graphdb.py
+++ db/graphdb.py
@@ -131,9 +131,10 @@ def template_function(labels_data, relationships_data):
 
 headlines = retrieve_headlines()
 headlines_search = []
-for headline in headlines:
-    headlines_search.append(headline['title'])
-tweeted_headlines = twitter_search_tweets_by_headlines(headlines_search)
+print(headlines)
+# for headline in headlines:
+#     headlines_search.append(headline['title'])
+tweeted_headlines = twitter_search_tweets_by_headlines(headlines)
 users = twitter_search_users_by_headlines(tweeted_headlines)
 # print(users[0])
 for user in users:
